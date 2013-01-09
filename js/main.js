9bf971f0d57577a4bb686b01aee51bb4dc3a0bb0
Xueqiao Xu <xueqiaoxu@gmail.com>
2013-01-09 16:07:58 +0800

    Fix style

--- js/main.js
+++ js/main.js
@@ -23,9 +23,8 @@
     },
     'contact': function () {
       loadFragment('#contact-content', function () {
-        $('#mail-address').html(
-          '<a href="mailto:xueqiaoxu@gmail.com">xueqiaoxu@gmail.com</a>'
-        );
+        $('#mail').attr('href', 'mailto:xueqiaoxu@gmail.com');
+        $('#mail-address').text('xueqiaoxu@gmail.com');
       });
     }
   };
